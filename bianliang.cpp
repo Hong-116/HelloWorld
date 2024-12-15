82bbd1f2243df08785bdbf50699313c76506818b
sbh011106@163.com <GitHub sbh011106@163.com>
2024-12-16 00:35:58 +0800

    变量

---
+++ bianliang.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+	变量存在的意义：方便我们管理存储空间
+	变量创建语法：数据类型  变量名  =  变量初始值；
+*/
+
+
+int main()
+{
+	int a = 10;
+
+	cout << "a = " << a << endl;
+
+
+	system("pause");
+	return 0;
+}
